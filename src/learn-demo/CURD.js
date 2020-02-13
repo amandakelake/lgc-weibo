6cce65b57ba0ba819aa2f763762fdc969ba7afdb
amandakelake <930007017@qq.com>
2020-02-13 20:48:50 +0800

    feat: sequelize exercise

---
+++ src/learn-demo/CURD.js
@@ -0,0 +1,59 @@
+const { Blog, User } = require('./sequelize');
+
+!(async function() {
+    // findOne类似  通过blog.dataValues取值
+    const blogPageList = await Blog.findAll({
+        limit: 2, // 限制本次查询
+        offset: 2, // 跳过多少条， 也就是分页
+        order: [['id', 'desc']],
+    });
+    console.log(
+        'blogPageList',
+        blogPageList.map(blog => blog.dataValues)
+    );
+
+    // 连表查询1  Blog.belongsTo(User, {}）
+    // findAndCountAll不考虑分页
+    const blogListWIthUser = await Blog.findAndCountAll({
+        order: [['id', 'desc']],
+        include: [
+            {
+                model: User,
+                attributes: ['userName', 'nickname'],
+                where: {
+                    userName: 'lgc',
+                },
+            },
+        ],
+    });
+    console.log(
+        'blogListWIthUser',
+        blogListWIthUser.count,
+        blogListWIthUser.rows.map(blog => {
+            const blogVal = blog.dataValues;
+            // 这里的blogVal.user是sequelize.define('user', {})时的名字
+            blogVal.user = blogVal.user.dataValues;
+            return blogVal;
+        })
+    );
+
+    // 连表查询2 User.hasMany(Blog, {})
+    const userListWithBlog = await User.findAndCountAll({
+        attributes: ['userName', 'nickname'],
+        include: [
+            {
+                model: Blog,
+            },
+        ],
+    });
+    console.log(
+        'userListWithBlog',
+        userListWithBlog.count,
+        userListWithBlog.rows.map(user => {
+            const userVal = user.dataValues;
+            // 一个用户可能有多个博客  所以user下面的字段叫blogs 而不是blog
+            userVal.blogs = userVal.blogs.map(blog => blog.dataValues)
+            return userVal;
+        })
+    );
+})();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,21 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2 -> lgc-weibo!'
-  })
-})
+    // 异步读取IO 需要await
+    await ctx.render('index', {
+        title: 'Hello Koa 2 -> lgc-weibo!',
+        msg: 'node backend project',
+    });
+});
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+    ctx.body = 'koa2 string';
+});
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    ctx.body = {
+        title: 'koa2 json',
+    };
+});
 
-module.exports = router
+module.exports = router;
